9ffb69d57725c9d2603d8168a94aba01cdd844b7
kudima03 <kudima03@gmail.com>
2026-02-16 21:09:05 +0300

    ChartConfiguration implemented

---
+++ src/Pure.Chart.RichRelationalModel.EFCore.Models.Configurations/ChartConfiguration.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pure.Primitives.Abstractions.EFCore.Converters;
+
+namespace Pure.Chart.RichRelationalModel.EFCore.Models.Configurations;
+
+public sealed record ChartConfiguration : IEntityTypeConfiguration<ChartEFCoreModel>
+{
+    public void Configure(EntityTypeBuilder<ChartEFCoreModel> builder)
+    {
+        _ = builder.HasKey(x => x.Id);
+
+        _ = builder
+            .Property(x => x.Id)
+            .ValueGeneratedNever()
+            .IsRequired()
+            .HasConversion(new GuidTypeConverter());
+
+        _ = builder
+            .Property(x => x.Title)
+            .IsRequired()
+            .HasConversion(new StringTypeConverter())
+            .HasMaxLength(64);
+
+        _ = builder
+            .Property(x => x.Description)
+            .IsRequired()
+            .HasConversion(new StringTypeConverter());
+
+        _ = builder
+            .Property(x => x.TypeId)
+            .IsRequired()
+            .HasConversion(new GuidTypeConverter());
+
+        _ = builder.HasOne(x => x.TypeNavigation).WithMany().HasForeignKey(x => x.TypeId);
+
+        _ = builder
+            .HasOne(x => x.XAxisNavigation)
+            .WithOne()
+            .HasForeignKey<AxisEFCoreModel>(x => x.ChartId);
+
+        _ = builder
+            .HasOne(x => x.YAxisNavigation)
+            .WithOne()
+            .HasForeignKey<AxisEFCoreModel>(x => x.ChartId);
+    }
+}
